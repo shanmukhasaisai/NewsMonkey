dcd7378af1b289f3937046dbe31cf5c831fa0359
shanmukhasai <shanmukhasaisai09@gmai.com>
2023-12-18 21:29:56 +0530

    rendering the news statically

---
+++ src/components/News.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export default class News extends Component {
+	articles = [
+		{
+			source: {
+				id: "bbc-sport",
+				name: "BBC Sport",
+			},
+			author: null,
+			title: "Lyon takes 500th Test wicket as Australia thrash Pakistan",
+			description:
+				"Australia off-spinner Nathan Lyon becomes the eighth player in Test cricket history to take 500 wickets.",
+			url: "http://www.bbc.co.uk/sport/cricket/67741224",
+			urlToImage:
+				"https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/11AF5/production/_132073427_lyon.jpg",
+			publishedAt: "2023-12-17T10:37:25.2208406Z",
+			content:
+				"Nathan Lyon is congratulated by his Australia teammates after taking his 500th career Test wicket against Pakistan in Perth\r\n<table>\r\n<tr><td>First Test, Perth Stadium (day four of five):</td></tr><t… [+3298 chars]",
+		},
+		{
+			source: {
+				id: "espn-cric-info",
+				name: "ESPN Cric Info",
+			},
+			author: null,
+			title:
+				"PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+			description:
+				"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+			url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+			urlToImage:
+				"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+			publishedAt: "2020-04-27T11:41:47Z",
+			content:
+				"Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
+		},
+		{
+			source: {
+				id: "espn-cric-info",
+				name: "ESPN Cric Info",
+			},
+			author: null,
+			title:
+				"What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+			description:
+				"Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+			url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+			urlToImage:
+				"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+			publishedAt: "2020-03-30T15:26:05Z",
+			content:
+				"Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
+		},
+	];
+	constructor() {
+		super();
+		this.state = {
+			articles: this.articles,
+			loading: false,
+		};
+	}
+
+	render() {
+		return (
+			<div className="container my-3">
+				<h2>NewsMonkey - Top Headlines</h2>
+				<div className="row">
+					{this.state.articles.map(ele => {
+						return (
+							<div className="col-md-4" key={ele.url}>
+								<NewsItem
+									title={ele.title}
+									description={ele.description}
+									imageUrl={ele.urlToImage}
+                  newsUrl={ele.url}
+								/>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class NewsItem extends Component {
+	render() {
+    let {title,description,imageUrl,newsUrl}=this.props;
+		return (
+			<div className="my-3">
+				<div className="card" Style="width: 18rem;" >
+					<img src={imageUrl} className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">{title}</h5>
+						<p className="card-text">
+							{description.slice(0.60)} ....
+						</p>
+						<a href={newsUrl} className="btn btn-sm btn-primary">
+							Read More...
+						</a>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
