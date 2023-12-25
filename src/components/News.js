bfba43c84aca8e3f1918481db8c9901f18a38d25
shanmukhasai <shanmukhasaisai09@gmai.com>
2023-12-25 13:12:19 +0530

    dynamically rendering the data and added the loading gif

--- src/components/News.js
+++ src/components/News.js
@@ -1,84 +1,88 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export default class News extends Component {
-	articles = [
-		{
-			source: {
-				id: "bbc-sport",
-				name: "BBC Sport",
-			},
-			author: null,
-			title: "Lyon takes 500th Test wicket as Australia thrash Pakistan",
-			description:
-				"Australia off-spinner Nathan Lyon becomes the eighth player in Test cricket history to take 500 wickets.",
-			url: "http://www.bbc.co.uk/sport/cricket/67741224",
-			urlToImage:
-				"https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/11AF5/production/_132073427_lyon.jpg",
-			publishedAt: "2023-12-17T10:37:25.2208406Z",
-			content:
-				"Nathan Lyon is congratulated by his Australia teammates after taking his 500th career Test wicket against Pakistan in Perth\r\n<table>\r\n<tr><td>First Test, Perth Stadium (day four of five):</td></tr><t… [+3298 chars]",
-		},
-		{
-			source: {
-				id: "espn-cric-info",
-				name: "ESPN Cric Info",
-			},
-			author: null,
-			title:
-				"PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-			description:
-				"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-			url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-			urlToImage:
-				"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-			publishedAt: "2020-04-27T11:41:47Z",
-			content:
-				"Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-		},
-		{
-			source: {
-				id: "espn-cric-info",
-				name: "ESPN Cric Info",
-			},
-			author: null,
-			title:
-				"What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-			description:
-				"Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-			url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-			urlToImage:
-				"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-			publishedAt: "2020-03-30T15:26:05Z",
-			content:
-				"Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-		},
-	];
 	constructor() {
 		super();
 		this.state = {
-			articles: this.articles,
+			articles: [],
 			loading: false,
+			page: 1,
 		};
 	}
+	async componentDidMount() {
+		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6f24d4fdace2445fa3ec2caaa8e33043&page=1&pageSize=${this.props.pageSize}`;
+		this.setState({loading:true});
+		let data = await fetch(url);
+		let parsedData = await data.json();
+		this.setState({ articles: parsedData.articles,
+			totalResults: parsedData.totalResults,
+			loading:false,
+
+		});
+	}
+	handleNextClick = async () => {
+		if (!(this.state.page + 1 >Math.ceil(this.state.totalResults / this.props.pageSize))) {
+			let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6f24d4fdace2445fa3ec2caaa8e33043&page=${
+				this.state.page + 1
+			}&pageSize=${this.props.pageSize}`;
+			this.setState({loading:true});
+			let data = await fetch(url);
+			let parsedData = await data.json();
+			this.setState({
+				page: this.state.page + 1,
+				articles: parsedData.articles,
+				loading:false,
+			});
+		}
+	};
+	handlePreClick = async () => {
+		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6f24d4fdace2445fa3ec2caaa8e33043&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+		this.setState({loading:true});
+		let data = await fetch(url);
+		let parsedData = await data.json();
+		this.setState({
+			page: this.state.page - 1,
+			articles: parsedData.articles,
+			loading:false,
+		});
+	};
 
 	render() {
 		return (
 			<div className="container my-3">
-				<h2>NewsMonkey - Top Headlines</h2>
+				<h1 className="text-center">NewsMonkey - Top Headlines</h1>
+				{this.state.loading&&<Spinner />}
 				<div className="row">
-					{this.state.articles.map(ele => {
+					{!this.state.loading && this.state.articles.map(ele => {
 						return (
 							<div className="col-md-4" key={ele.url}>
 								<NewsItem
 									title={ele.title}
-									description={ele.description}
-									imageUrl={ele.urlToImage}
-                  newsUrl={ele.url}
+									description={
+										ele.description ? ele.description.slice(0, 100) : ""
+									}
+									imageUrl={
+										ele.urlToImage? ele.urlToImage: "https://imgeng.jagran.com/images/2023/dec/free-fire-max-redeem-codes-2023-india-today1703395333665.jpg"
+									}
+									newsUrl={ele.url}
 								/>
 							</div>
 						);
 					})}
+				</div>
+				<div className="container d-flex justify-content-between">
+					<button
+						disabled={this.state.page <= 1}
+						type="button"
+						className="btn btn-primary"
+						onClick={this.handlePreClick}
+					>
+						{" "}
+						&larr; Previous
+					</button>
+					<button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
 				</div>
 			</div>
 		);
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,12 +5,12 @@ export default class NewsItem extends Component {
     let {title,description,imageUrl,newsUrl}=this.props;
 		return (
 			<div className="my-3">
-				<div className="card" Style="width: 18rem;" >
+				<div className="card" style={{width: '18rem'}} >
 					<img src={imageUrl} className="card-img-top" alt="..." />
 					<div className="card-body">
 						<h5 className="card-title">{title}</h5>
 						<p className="card-text">
-							{description.slice(0.60)} ....
+							{description} ....
 						</p>
 						<a href={newsUrl} className="btn btn-sm btn-primary">
 							Read More...
